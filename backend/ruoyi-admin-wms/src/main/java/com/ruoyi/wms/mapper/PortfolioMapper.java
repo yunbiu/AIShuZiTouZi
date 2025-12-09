832445d7d50cf511d5baa79802d240bb64d8eec7
buluobuluo0521 <1803053907@qq.com>
2025-12-09 09:23:22 +0800

    修正持仓历史记录与sql字段 曾畅茂

--- backend/ruoyi-admin-wms/src/main/java/com/ruoyi/wms/mapper/PortfolioMapper.java
+++ backend/ruoyi-admin-wms/src/main/java/com/ruoyi/wms/mapper/PortfolioMapper.java
@@ -2,6 +2,14 @@ package com.ruoyi.wms.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ruoyi.wms.domain.entity.Portfolio;
+import com.ruoyi.wms.domain.query.PortfolioQuery;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface PortfolioMapper extends BaseMapper<Portfolio> {
+
+    //查询当前持仓
+    List<PortfolioQuery> getPortfolioList(@Param("userId") Integer userId);
+
 }
--- backend/ruoyi-admin-wms/src/main/java/com/ruoyi/wms/service/impl/PortfolioServiceImpl.java
+++ backend/ruoyi-admin-wms/src/main/java/com/ruoyi/wms/service/impl/PortfolioServiceImpl.java
@@ -3,6 +3,7 @@ package com.ruoyi.wms.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.common.core.domain.R;
 import com.ruoyi.wms.domain.entity.Portfolio;
+import com.ruoyi.wms.domain.query.PortfolioQuery;
 import com.ruoyi.wms.mapper.PortfolioMapper;
 import com.ruoyi.wms.service.PortfolioService;
 import org.springframework.stereotype.Service;
@@ -12,8 +13,9 @@ import java.util.List;
 @Service
 public class PortfolioServiceImpl extends ServiceImpl<PortfolioMapper, Portfolio> implements PortfolioService {
 
+
     @Override
-    public List<Portfolio> getPortfolioList() {
-        return this.list();
+    public List<PortfolioQuery> getPortfolioList(Integer userId) {
+        return baseMapper.getPortfolioList(userId);
     }
 }
